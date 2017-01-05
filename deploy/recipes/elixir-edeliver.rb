5bd5bc424b29fc3214e763a74fe9c42f9916b7ad
Dmitry Trager <dmitry.trager@flatstack.com>
2017-01-05 17:00:25 +0300

    Add recipe for deploy elixir app via edeliver

---
+++ deploy/recipes/elixir-edeliver.rb
@@ -0,0 +1,31 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+
+  script "update_ssh_config" do
+    interpreter "bash"
+    user "ubuntu"
+    code <<-EOH
+
+    echo "Host ec2timelapse" > /home/ubuntu/.ssh/config
+    echo "Hostname localhost" > /home/ubuntu/.ssh/config
+    echo "User ubuntu" > /home/ubuntu/.ssh/config
+
+    EOH
+  end
+
+  execute "get local hex" do
+    command "mix local.hex --force"
+    action :run
+  end
+
+  execute "setup application locally" do
+    command "bin/setup"
+    action :run
+  end
+
+  execute "deploy with edeliver" do
+    command "bin/deploy staging"
+    action :run
+  end
+end
