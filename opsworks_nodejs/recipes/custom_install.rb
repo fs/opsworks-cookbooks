06ae2330f8ddc15bdf0e865f0c6011f5c1406fca
Dmitry Trager <dmitry.trager@flatstack.com>
2017-01-17 19:57:20 +0300

    Use one script for nodejs install instead of 3 exec blocks

--- opsworks_nodejs/recipes/custom_install.rb
+++ opsworks_nodejs/recipes/custom_install.rb
@@ -12,36 +12,36 @@ when "ubuntu","debian"
     end
   end
 
-  # script "install_nodejs_via_nvm" do
-  #   interpreter "bash"
-  #   user "ubuntu"
-  #   code <<-EOH
-  #     cd /home/ubuntu
+  script "install_nodejs_via_nvm" do
+    interpreter "bash"
+    user "ubuntu"
+    code <<-EOH
+      cd /home/ubuntu
 
-  #     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 
-  #     echo "export NVM_DIR=\"/home/ubuntu/.nvm\"" >> /home/ubuntu/.bashrc
-  #     echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" >> /home/ubuntu/.bashrc
+      echo "export NVM_DIR=\"/home/ubuntu/.nvm\"" >> /home/ubuntu/.bashrc
+      echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" >> /home/ubuntu/.bashrc
 
-  #     source /home/ubuntu/.nvm/nvm.sh
-  #     nvm install 6.5.0
-  #   EOH
+      source /home/ubuntu/.nvm/nvm.sh
+      nvm install 6.5.0
+    EOH
+  end
+
+  # execute "install nvm" do
+  #   command "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash"
+  #   action :run
   # end
 
-  execute "install nvm" do
-    command "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash"
-    action :run
-  end
+  # execute "activate nvm" do
+  #   command ". ~/.nvm/nvm.sh"
+  #   action :run
+  # end
 
-  execute "activate nvm" do
-    command ". ~/.nvm/nvm.sh"
-    action :run
-  end
-
-  execute "install nodejs" do
-    command "nvm install 6.5.0"
-    action :run
-  end
+  # execute "install nodejs" do
+  #   command "nvm install 6.5.0"
+  #   action :run
+  # end
 else
   raise "Your platform `#{node[:platform]}` (family: `#{node[:platform_family]}`) is not supported!"
 end
