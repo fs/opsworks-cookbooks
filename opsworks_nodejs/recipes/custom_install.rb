f36e65d4aa5c8d472c5de7adeed3029ce1cd025e
Dmitry Trager <dmitry.trager@flatstack.com>
2017-01-17 19:46:38 +0300

    Install some packages before nvm install

--- opsworks_nodejs/recipes/custom_install.rb
+++ opsworks_nodejs/recipes/custom_install.rb
@@ -2,6 +2,32 @@ log "[#{cookbook_name}][#{recipe_name}] Running ..."
 
 case node[:platform_family]
 when "ubuntu","debian"
+  execute "apt-get update" do
+    action :run
+  end
+
+  %w(build-essential libssl-dev).each do |pkg|
+    package pkg do
+      action :install
+    end
+  end
+
+  # script "install_nodejs_via_nvm" do
+  #   interpreter "bash"
+  #   user "ubuntu"
+  #   code <<-EOH
+  #     cd /home/ubuntu
+
+  #     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+  #     echo "export NVM_DIR=\"/home/ubuntu/.nvm\"" >> /home/ubuntu/.bashrc
+  #     echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" >> /home/ubuntu/.bashrc
+
+  #     source /home/ubuntu/.nvm/nvm.sh
+  #     nvm install 6.5.0
+  #   EOH
+  # end
+
   execute "install nvm" do
     command "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash"
     action :run
