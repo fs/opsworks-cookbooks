44b43eda98d7ff02e657864c88c56947857e1987
Dmitry Trager <dmitry.trager@flatstack.com>
2017-01-18 17:59:55 +0300

    Add custom actions into default nodejs deploy recipe

--- deploy/recipes/nodejs.rb
+++ deploy/recipes/nodejs.rb
@@ -1,11 +1,6 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'nodejs'
-    Chef::Log.debug("Skipping deploy::nodejs for application #{application} as it is not a node.js app")
-    next
-  end
-
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
@@ -20,6 +15,32 @@ node[:deploy].each do |application, deploy|
   opsworks_nodejs do
     deploy_data deploy
     app application
+  end
+
+  script "install_nodejs_via_nvm" do
+    interpreter "bash"
+    code <<-EOH
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+      source ~/.nvm/nvm.sh
+
+      nvm install 6.5.0
+    EOH
+
+    not_if do
+      File.exist?("~/.nvm/nvm.sh")
+    end
+  end
+
+  execute "change npm registry" do
+    command "npm config set registry http://registry.npmjs.org/"
+    action :run
+  end
+
+  execute "setup application locally" do
+    cwd "#{deploy[:deploy_to]}/current"
+    command "nvm use 6.5.0 && bin/setup"
+    action :run
   end
 
   application_environment_file do
