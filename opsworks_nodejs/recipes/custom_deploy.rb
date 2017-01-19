106e52933a6846057c1298ab384ea5955479a87a
Dmitry Trager <dmitry.trager@flatstack.com>
2017-01-19 11:36:17 +0300

    Rollback changes for built-in recipe, move custom deploy recipe into opsworks_nodejs recipe list

--- opsworks_nodejs/recipes/custom_deploy.rb
+++ opsworks_nodejs/recipes/custom_deploy.rb
@@ -1,11 +1,6 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'nodejs'
-    Chef::Log.debug("Skipping deploy::nodejs for application #{application} as it is not a node.js app")
-    next
-  end
-
   opsworks_deploy_dir do
     user deploy[:user]
     group deploy[:group]
@@ -27,6 +22,37 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
     path ::File.join(deploy[:deploy_to], "shared")
     environment_variables deploy[:environment_variables]
+  end
+
+  bash "install_nodejs_via_nvm" do
+    user "root"
+    code <<-EOH
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+      source ~/.nvm/nvm.sh
+
+      nvm install 6.5.0
+    EOH
+
+    not_if do
+      File.exist?("~/.nvm/nvm.sh")
+    end
+  end
+
+  execute "change npm registry" do
+    command "npm config set registry http://registry.npmjs.org/"
+    action :run
+  end
+
+  bash "setup application locally" do
+    cwd "#{deploy[:deploy_to]}/current"
+    user "root"
+    code <<-EOH
+      source ~/.nvm/nvm.sh
+      nvm use 6.5.0
+
+      bin/setup
+    EOH
   end
 
   ruby_block "restart node.js application #{application}" do
