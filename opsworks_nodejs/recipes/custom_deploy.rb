5dcb08a6e376b86fee8b68703df2e2b1addf4beb
Dmitry Trager <dmitry.trager@flatstack.com>
2017-01-21 12:59:49 +0300

    Make a symlink for new version of nodejs

--- opsworks_nodejs/recipes/custom_deploy.rb
+++ opsworks_nodejs/recipes/custom_deploy.rb
@@ -35,41 +35,16 @@ node[:deploy].each do |application, deploy|
     action :run
   end
 
+  Chef::Log.info("Make a symlink for new version of nodejs")
+  execute "Symlink new nodejs" do
+    command "rm /usr/local/bin/node && ln -s /usr/bin/nodejs /usr/local/bin/node"
+    action :run
+  end
+
   Chef::Log.info("Local application setup via bin/setup")
   execute "bin/setup" do
     cwd "#{deploy[:deploy_to]}/current"
   end
-
-  # ruby_block "change HOME to #{deploy[:home]} for local setup" do
-  #   block do
-  #     ENV['HOME'] = "#{deploy[:home]}"
-  #   end
-  # end
-
-  # Chef::Log.info("Install nvm from source")
-  # execute "Install nvm" do
-  #   command "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash"
-  #   action :run
-
-  #   not_if do
-  #     File.exist?("~/.nvm/nvm.sh")
-  #   end
-  # end
-
-  # Chef::Log.info("Install nodejs 6.5.0 via nvm")
-  # execute ". ~/.nvm/nvm.sh && nvm install 6.5.0 && nvm alias default node" do
-  # end
-
-  # Chef::Log.info("Local application setup via bin/setup")
-  # execute ". ~/.nvm/nvm.sh && npm --prefix="" set prefix "" && nvm use --delete-prefix v6.5.0 --silent && bin/setup" do
-  #   cwd "#{deploy[:deploy_to]}/current"
-  # end
-
-  # ruby_block "change HOME back to /root after local setup" do
-  #   block do
-  #     ENV['HOME'] = "/root"
-  #   end
-  # end
 
   ruby_block "restart node.js application #{application}" do
     block do
