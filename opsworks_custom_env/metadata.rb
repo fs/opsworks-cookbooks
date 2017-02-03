84febe7c3afbb15045e143d312e97e41135d6df7
Dmitry Trager <dmitry.trager@flatstack.com>
2017-02-03 21:58:29 +0300

    Add cookbook for setting of custom env

---
+++ opsworks_custom_env/metadata.rb
@@ -0,0 +1,5 @@
+name        "opsworks_custom_env"
+maintainer  "Diego Durante"
+description "This recipe allow you to use environment variables inside your Rails app when you use a AWS OpsWorks stack."
+
+recipe "opsworks_custom_env::default", "Generate a config/application.yml file to contain all environment variables"
---
+++ opsworks_custom_env/recipes/default.rb
@@ -0,0 +1,13 @@
+# Recipe used for a setup and deploy events
+Chef::Log.info("Create config/application.yml file...")
+
+application = node[:rails][:application_name]
+deploy = node[:deploy][application]
+
+environment_variables = deploy[:custom_env].to_h.merge(deploy[:environment_variables].to_h)
+
+custom_env_template do
+  application application
+  deploy deploy
+  env environment_variables
+end
