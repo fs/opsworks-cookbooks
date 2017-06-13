1fffb0ea67ad0eff95c7e7d9134f1577e1e0bde4
Dmitry Trager <dmitry.trager@flatstack.com>
2017-06-13 18:05:03 +0300

    Add service restart to puma deploy recipe

--- opsworks_puma/recipes/deploy.rb
+++ opsworks_puma/recipes/deploy.rb
@@ -4,12 +4,21 @@ deploy = node[:deploy][application]
 opsworks_deploy_user do
   deploy_data deploy
 end
+
 opsworks_deploy_dir do
   user deploy[:user]
   group deploy[:group]
   path deploy[:deploy_to]
 end
+
 puma_web_app do
   application application
   deploy deploy
 end if deploy[:puma]
+
+service application do
+  if node[:opsworks][:activity] == "deploy"
+    action :restart
+    restart_command "service #{application} restart"
+  end
+end
